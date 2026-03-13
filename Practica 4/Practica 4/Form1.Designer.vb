bcb770bd9af0814be51209f5f7902c3cbc8775dd
luis-torresugarte <luis.torresugarte@iest.edu.mx>
2026-03-13 08:33:15 -0600

    Agregamos un panel de configuracion y un diseño

--- Practica 4/Practica 4/Form1.Designer.vb
+++ Practica 4/Practica 4/Form1.Designer.vb
@@ -29,43 +29,60 @@ Partial Class Form1
         Me.dias = New System.Windows.Forms.Label()
         Me.fecha = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Config = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.Config, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'horas
         '
         Me.horas.AutoSize = True
-        Me.horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.horas.Location = New System.Drawing.Point(34, 65)
+        Me.horas.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.horas.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.horas.Location = New System.Drawing.Point(396, 194)
         Me.horas.Name = "horas"
-        Me.horas.Size = New System.Drawing.Size(215, 153)
+        Me.horas.Size = New System.Drawing.Size(107, 76)
         Me.horas.TabIndex = 0
         Me.horas.Text = "00"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(255, 65)
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(520, 194)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(102, 153)
+        Me.Label2.Size = New System.Drawing.Size(52, 76)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = ":"
         '
         'minutos
         '
         Me.minutos.AutoSize = True
-        Me.minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.minutos.Location = New System.Drawing.Point(340, 65)
+        Me.minutos.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.minutos.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.minutos.Location = New System.Drawing.Point(590, 194)
         Me.minutos.Name = "minutos"
-        Me.minutos.Size = New System.Drawing.Size(215, 153)
+        Me.minutos.Size = New System.Drawing.Size(107, 76)
         Me.minutos.TabIndex = 2
         Me.minutos.Text = "00"
         '
         'dias
         '
         Me.dias.AutoSize = True
+        Me.dias.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.dias.Font = New System.Drawing.Font("Microsoft Sans Serif", 35.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.dias.Location = New System.Drawing.Point(51, 218)
+        Me.dias.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.dias.Location = New System.Drawing.Point(467, 285)
         Me.dias.Name = "dias"
         Me.dias.Size = New System.Drawing.Size(151, 54)
         Me.dias.TabIndex = 3
@@ -74,28 +91,91 @@ Partial Class Form1
         'fecha
         '
         Me.fecha.AutoSize = True
-        Me.fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.fecha.Location = New System.Drawing.Point(52, 292)
+        Me.fecha.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 23.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.fecha.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.fecha.Location = New System.Drawing.Point(387, 359)
         Me.fecha.Name = "fecha"
-        Me.fecha.Size = New System.Drawing.Size(404, 46)
+        Me.fecha.Size = New System.Drawing.Size(310, 35)
         Me.fecha.TabIndex = 4
         Me.fecha.Text = "09 de Marzo del 2026"
         '
         'Timer1
         '
         '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Config
+        '
+        Me.Config.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Config.Image = Global.Practica_4.My.Resources.Resources.descarga1
+        Me.Config.Location = New System.Drawing.Point(627, 409)
+        Me.Config.Name = "Config"
+        Me.Config.Size = New System.Drawing.Size(70, 62)
+        Me.Config.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Config.TabIndex = 6
+        Me.Config.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Practica_4.My.Resources.Resources.reloj
+        Me.PictureBox1.Location = New System.Drawing.Point(-28, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1152, 864)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(393, 170)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(294, 224)
+        Me.Panel1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(182, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Configuración"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(70, 22)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(603, 379)
+        Me.ClientSize = New System.Drawing.Size(1069, 752)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Config)
         Me.Controls.Add(Me.fecha)
         Me.Controls.Add(Me.dias)
         Me.Controls.Add(Me.minutos)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.horas)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Reloj Digital"
+        CType(Me.Config, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -107,4 +187,10 @@ Partial Class Form1
     Friend WithEvents dias As Label
     Friend WithEvents fecha As Label
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Config As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Practica 4/Practica 4/Form1.vb
+++ Practica 4/Practica 4/Form1.vb
@@ -15,4 +15,20 @@
         dias.Text = Day_o
         fecha.Text = Date_o
     End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label2.Text = ":" Then
+            Label2.Text = ""
+        ElseIf label2.text = "" Then
+            Label2.Text = ":"
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles Config.Click
+        If Panel1.Visible = False Then
+            Panel1.Visible = True
+        ElseIf Panel1.Visible = True Then
+            Panel1.Visible = False
+        End If
+    End Sub
 End Class
